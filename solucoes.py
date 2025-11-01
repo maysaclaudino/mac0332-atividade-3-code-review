c08164c010721b4b7bf06ed674d65f693113c9d3
Lais Nuto Rossman <laisnutorossman@gmail.com>
2025-11-01 14:30:55 -0300

    normalize string inputs

--- solucoes.py
+++ solucoes.py
@@ -1,24 +1,26 @@
 def sao_anagramas(string1, string2):
-
-   
+    
+    string1_normalizada = string1.replace(" ", "").lower()
+    string2_normalizada = string2.replace(" ", "").lower()
+    
     contagem1 = {}
     contagem2 = {}
     
-    
-    for letra in string1:
+  
+    for letra in string1_normalizada:
         if letra in contagem1:
             contagem1[letra] = contagem1[letra] + 1
         else:
             contagem1[letra] = 1
     
     
-    for letra in string2:
+    for letra in string2_normalizada:
         if letra in contagem2:
             contagem2[letra] = contagem2[letra] + 1
         else:
             contagem2[letra] = 1
     
-    
+   
     return contagem1 == contagem2
 
 def cifra_de_cesar(texto, deslocamento):
